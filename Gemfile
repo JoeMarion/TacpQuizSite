2981e9d8455a75047b86be116616e5ffb42b9c4f
= <joeyzmarion@gmail.com>
2016-06-07 21:10:03 -0500

    Initialize repo

---
+++ Gemfile
@@ -0,0 +1,61 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.2.5'
+gem 'pg', '~> 0.15'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'bcrypt'
+gem 'susy'
+gem 'breakpoint'
+gem 'compass-rails'
+gem 'figaro'
+gem 'haml', '~> 4.0.5'
+gem 'haml-rails'
+gem 'normalize-rails'
+gem 'faker'
+gem 'will_paginate', '~> 3.1.0'
+# gem 'afterparty'
+# gem 'coderay'
+# gem 'redcarpet'
+# gem 'carrierwave'
+# gem 'carrierwave_direct'
+# gem 'sidekiq'
+# gem 'mini_magick'
+# gem 'fog'
+# gem 'ckeditor_rails'
+
+
+group :test do
+  gem "capybara"
+  gem "selenium-webdriver"
+  gem "connection_pool"
+  gem "launchy"
+  gem "minitest-reporters"
+  gem "mocha"
+  gem "poltergeist"
+  gem "shoulda-context"
+  gem "shoulda-matchers", ">= 3.0.1"
+  gem "test_after_commit"
+end
+
+group :development, :test do
+  gem 'byebug'
+end
+
+group :development do
+  gem 'web-console', '~> 2.0'
+  gem 'spring'
+  gem "guard", ">= 2.2.2", :require => false
+  gem "guard-minitest", :require => false
+  gem "rb-fsevent", :require => false
+  gem "terminal-notifier-guard", :require => false
+end
+
+group :production do
+  gem 'puma'
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,60 @@
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
+require "mocha/mini_test"
+
+# Improved Minitest output (color and progress bar)
+require "minitest/reporters"
+Minitest::Reporters.use!(
+  Minitest::Reporters::ProgressReporter.new,
+  ENV,
+  Minitest.backtrace_filter)
+
+# Capybara and poltergeist integration
+require "capybara/rails"
+require "capybara/poltergeist"
+Capybara.javascript_driver = :poltergeist
+
+class ActiveSupport::TestCase
+  fixtures :all
+
+  # Returns true if a test user is logged in
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  # Logs in a test user.
+  def log_in_as(user, options = {})
+    password = options[:password]       || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { email: user.email,
+                                  password: password,
+                                  remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+    # Returns true inside an integration test.
+    def integration_test?
+      defined?(post_via_redirect)
+    end
+end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+end
+
+# See: https://gist.github.com/mperham/3049152
+class ActiveRecord::Base
+  mattr_accessor :shared_connection
+  @@shared_connection = nil
+
+  def self.connection
+    @@shared_connection || ConnectionPool::Wrapper.new(:size => 1) { retrieve_connection }
+  end
+end
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
