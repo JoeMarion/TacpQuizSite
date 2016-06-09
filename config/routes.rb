579311a35ea940eae9a036f9da3b5cc3820b3d5d
= <joeyzmarion@gmail.com>
2016-06-09 01:26:29 -0500

    Remove topics & post. Pass tests for user authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
 
+  root to: 'sessions#new'
+
   get    'signup' => 'users#new'
   get    'login'  => 'sessions#new'
   post   'login'  => 'sessions#create'
